6bda5a104b1923caf7207d1673c8145076681671
0xGRAV3R <ycopyart.xyz@gmail.com>
2023-08-27 15:41:38 -0700

    insert or update values in the hash map using the insert method

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,8 @@ use std::collections::HashMap;
 fn main() {
     let mut my_hash_map: HashMap<String, i32> = HashMap::new();
     
+    my_hash_map.insert(String::from("key1"), 42);
+    my_hash_map.insert(String::from("key2"), 87);
+
     println!("Hello, hashmaps!");
 }
