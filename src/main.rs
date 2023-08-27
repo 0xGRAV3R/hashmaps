36ba617fab54183066590dffc38ab7770c987dd4
0xGRAV3R <ycopyart.xyz@gmail.com>
2023-08-27 15:45:16 -0700

    made some checks and testing

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 
 fn main() {
     let mut my_hash_map: HashMap<String, i32> = HashMap::new();
-    
+
     my_hash_map.insert(String::from("key1"), 42);
     my_hash_map.insert(String::from("key2"), 87);
 
@@ -23,5 +23,9 @@ fn main() {
     } else {
         println!("Key does not exist");
     }
-
 }
+
+
+
+
+
